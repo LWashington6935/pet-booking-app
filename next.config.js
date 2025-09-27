b51a134a0343f9169a186f9ec22963a7069ccd15
Lucas Washington <LWashington6935@gmail.com>
2025-09-26 20:38:14 -0400

    revanmp

--- next.config.js
+++ next.config.js
@@ -1,12 +1,5 @@
 /** @type {import('next').NextConfig} */
-const isProduction = process.env.NODE_ENV === 'production';
-const isProjectPage = true;
-const repoName = 'pet-booking-app';
-
 module.exports = {
-  output: 'export',
   images: { unoptimized: true },
-  basePath: isProduction && isProjectPage ? `/${repoName}` : '',
-  assetPrefix: isProduction && isProjectPage ? `/${repoName}/` : '',
   trailingSlash: true,
 };
